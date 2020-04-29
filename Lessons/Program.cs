84c9223100df22dc740b2209abc05ee1f3abdb19
Hadar Nir <hadarnir33@gmail.com>
2020-04-29 22:12:38 +0300

    Added classes to solution and changed the namescpaces in each class.

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lessons;
 
 namespace Lessons
 {
@@ -10,6 +11,7 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
+          
         }
     }
 }
