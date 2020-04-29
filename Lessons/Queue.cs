d71ccfe2106eda13fe4db31eacbe55024f38ad21
Hadar Nir <hadarnir33@gmail.com>
2020-04-29 21:47:44 +0300

    Added data structures to project.

---
+++ Lessons/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOfListAndGenericList
+{
+    class Queue<T>
+    {
+        private Node<T> first;
+        private Node<T> last;
+
+
+        /* הפעולה בונה ומחזירה תור ריק **/
+        public Queue()
+        {
+            this.first = null;
+            this.last = null;
+        }
+        /* הפעולה מכניסה את הערך x לסוף התור הנוכחי **/
+        public void Insert(T x)
+        {
+            Node<T> temp = new Node<T>(x);
+            if (first == null)
+                first = temp;
+            else
+                last.SetNext(temp);
+            last = temp;
+        }
+        /* הפעולה מוציאה ומחזירה את הערך הנמצא  בראש התור הנוכחי **/
+        public T Remove()
+        {
+            T x = first.GetInfo();
+            first = first.GetNext();
+            if (first == null)
+                last = null;
+            return x;
+        }
+        /* הפעולה מחזירה את הערך הנמצא  בראש התור הנוכחי **/
+        public T Head()
+        {
+            return first.GetInfo();
+        }
+        /* הפעולה מחזירה אמת אם התור הנוכחי ריק או שקר אחרת **/
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+        /* הפעולה מחזירה מחרוזת המתארת את התור הנוכחי */
+        public override string ToString()
+        {
+            string s = "[";
+            Node<T> p = this.first;
+            while (p != null)
+            {
+                s = s + p.GetInfo().ToString();
+                if (p.GetNext() != null)
+                    s = s + ",";
+                p = p.GetNext();
+            }
+            s = s + "]";
+            return s;
+        }
+    }
+}
---
+++ Lessons/Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOfListAndGenericList
+{
+    class Stack<T>
+    {
+        private Node<T> head;
+        /* הפעולה בונה ומחזירה מחסנית ריקה **/
+        public Stack()
+        {
+            this.head = null;
+        }
+        public void Push(T x)
+        {
+            Node<T> temp = new Node<T>(x);
+            temp.SetNext(head);
+            head = temp;
+        }
+        /* הפעולה מכניסה את הערך x לראש המחסנית הנוכחית **/
+        /* הפעולה מוציאה ומחזירה את הערך הנמצא  בראש המחסנית הנוכחית **/
+        public T Pop()
+        {
+            T x = head.GetInfo();
+            head = head.GetNext();
+            return x;
+        }
+
+
+        /* הפעולה מחזירה את הערך הנמצא  בראש המחסנית הנוכחית **/
+        public T Top()
+        {
+            return head.GetInfo();
+        }
+
+
+        /* הפעולה מחזירה 'אמת' אם המחסנית הנוכחית ריקה, ומחזירה 'שקר' אחרת **/
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+
+        /* הפעולה מחזירה מחרוזת המתארת את המחסנית הנוכחית */
+        public override string ToString()
+        {
+            string s = "[";
+            Node<T> p = this.head;
+            while (p != null)
+            {
+                s = s + p.GetInfo().ToString();
+                if (p.GetNext() != null)
+                    s = s + ",";
+                p = p.GetNext();
+            }
+            s = s + "]";
+            return s;
+        }
+    }
+}
