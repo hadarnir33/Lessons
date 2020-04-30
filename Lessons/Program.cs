cdf713877e0e7bb28d9a4a1ae0a1e6a3315e8711
hadarnir33 <hadarnir33@gmail.com>
2020-04-30 16:11:11 +0300

    Finished 2018 exam ex 4.

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -11,41 +11,59 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            Stack<int> S = generateStackByInput();
-            Console.WriteLine(S);
-            Console.WriteLine(lastAndRemove(S));
-            Console.WriteLine(S);
+            Stack<int> s = generateStackByInput();
+            Console.WriteLine(s);      
+            Stack<TwoItems> stkTwoItems= stackTwoItems(s);
+            Console.WriteLine(stkTwoItems);
+
+            
         }
 
         // This function creates a stack according to the user inputs
         public static Stack<int> generateStackByInput()
         {
-            Stack<int> S = new Stack<int>();
+            Stack<int> s = new Stack<int>();
             string input;
             Console.WriteLine("Enter numbers for the stack, enter -1 to finish");
             input = Console.ReadLine();
             while(input != "-1")
             {
-                S.Push(int.Parse(input));
+                s.Push(int.Parse(input));
                 input = Console.ReadLine();
             }
-            return S;
+            return s;
         }
 
         // Exam 2018: 4.a, this function returns the number in the bottom of the stack and remove it from the stack.
-        public static int lastAndRemove(Stack<int> S)
+        public static int lastAndRemove(Stack<int> s)
         {
-            Stack<int> Temp = new Stack<int>();
+            Stack<int> temp = new Stack<int>();
             int bottomOfStack = 0;
-            while (!S.IsEmpty())
+            while (!s.IsEmpty())
             {
-                bottomOfStack = S.Pop();
-                if (!S.IsEmpty())
-                    Temp.Push(bottomOfStack);
+                bottomOfStack = s.Pop();
+                if (!s.IsEmpty())
+                    temp.Push(bottomOfStack);
             }
-            while (!Temp.IsEmpty())
-                S.Push(Temp.Pop());
+            while (!temp.IsEmpty())
+                s.Push(temp.Pop());
             return bottomOfStack;
+        }
+
+        // Exam 2018: 4.b, this function creates a TwoItems stack from a given stack with even length.
+        public static Stack<TwoItems> stackTwoItems(Stack<int> stk1)
+        {
+            Stack < TwoItems > stkTwoItems= new Stack<TwoItems>();
+            TwoItems item;
+            int firstNumber, secondNumber;
+            while (!stk1.IsEmpty())
+            {
+                firstNumber = stk1.Pop();
+                secondNumber = lastAndRemove(stk1);
+                item = new TwoItems(firstNumber, secondNumber);
+                stkTwoItems.Push(item);
+            }
+            return stkTwoItems;
         }
     }
 }
---
+++ Lessons/TwoItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    // Exam 2018: 4.b
+    class TwoItems
+    {
+        private int num1;
+        private int num2;
+
+
+        public TwoItems(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public int Num1 { get => num1; set => num1 = value; }
+        public int Num2 { get => num2; set => num2 = value; }
+
+        public override string ToString()
+        {
+            string s = "[" + this.num1 + "," + this.num2 + "]";
+            return s;
+        }
+    }
+}
