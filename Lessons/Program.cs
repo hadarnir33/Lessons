f5fa26162031d32b40c8cc5e592c270816162ec3
Hadar Nir <hadarnir33@gmail.com>
2020-05-15 14:37:37 +0300

    Added BuildNodeChain function.

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -11,7 +11,26 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-          
+            Node<int> n = BuildNodeChain();
+            Console.WriteLine(n);
+        }
+
+        public static Node<int> BuildNodeChain()
+        {
+            Node<int> n1 = new Node<int>(1);
+            Node<int> n2 = new Node<int>(2);
+            Node<int> n3 = new Node<int>(4);
+            Node<int> n4 = new Node<int>(5);
+            Node<int> n5 = new Node<int>(6);
+            Node<int> n6 = new Node<int>(10);
+            Node<int> n7 = new Node<int>(11);
+            n1.SetNext(n2);
+            n2.SetNext(n3);
+            n3.SetNext(n4);
+            n4.SetNext(n5);
+            n5.SetNext(n6);
+            n6.SetNext(n7);
+            return n1;
         }
     }
 }
