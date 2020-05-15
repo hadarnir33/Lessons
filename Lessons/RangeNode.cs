c50280df3631a6a8d1017c49692a1de6721ed302
Hadar Nir <hadarnir33@gmail.com>
2020-05-15 14:40:03 +0300

    Added RangeNode class

---
+++ Lessons/RangeNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class RangeNode
+    {
+        private int from;
+        private int to;
+
+
+        public RangeNode(int low, int high)
+        {
+            this.from = low;
+            this.to = high;
+        }
+
+        public int Low { get => from; set => from = value; }
+        public int High { get => to; set => to = value; }
+
+        public override string ToString()
+        {
+            string s = "[" + this.from + "," + this.to + "]";
+            return s;
+        }
+    }
+}
